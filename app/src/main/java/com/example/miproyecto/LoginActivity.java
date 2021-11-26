325ecf52b1a969978ee17443a882d74ff468355d
CAROLINA <dycaro750@gmail.com>
2021-11-25 23:03:38 -0500

    sharedPreference

--- app/src/main/java/com/example/miproyecto/LoginActivity.java
+++ app/src/main/java/com/example/miproyecto/LoginActivity.java
@@ -20,16 +20,23 @@ import com.example.miproyecto.Adapters.EditProductsActivity;
 import com.example.miproyecto.Adapters.ProductAdapter;
 import com.example.miproyecto.databinding.ActivityLoginBinding;
 import com.example.miproyecto.databinding.ActivityMainBinding;
+import com.example.miproyecto.entities.PreferenceEntity;
 import com.example.miproyecto.entities.ProductEntity;
+import com.example.miproyecto.entities.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -42,21 +49,25 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
+    private FirebaseUser user;
+    private ArrayList<Usuario> arrayList;
+    private  Usuario usuario;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_login);
+        PreferenceEntity.setContext (this);
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
-        saveUserPreferences(this);
+        arrayList = new ArrayList<> ();
+
 
         etemail = findViewById (R.id.etemail);
         etpassword = findViewById (R.id.etpassword);
 
     }
-
 
 
     public void login(View view) {
@@ -80,11 +91,7 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful ()) {
-                                Toast.makeText (getApplicationContext (),
-                                        "Login exitoso", Toast.LENGTH_SHORT).show ();
-                                Intent intent = new Intent (getApplicationContext (), ListProductsActivity.class);
-                                startActivity(intent);
-                                saveUserPreferences (getApplicationContext ());
+                                consulta ();
                             } else {
                                 Toast.makeText (getApplicationContext (), "Login Fallido", Toast.LENGTH_SHORT).show ();
                             }
@@ -93,22 +100,35 @@ public class LoginActivity extends AppCompatActivity {
 
         }
     }
-    private void saveUserPreferences(Context context ) {
-        db.collection ("products");
-        String email = "dycaro750@gmail.com";
-        String rol = "";
-        String name = "carolina";
+    public void  consulta(){
+        String email=etemail.getText ().toString ();
+        db.collection ("artesanalwine").whereEqualTo ("email",email)
+                .get ().addOnCompleteListener (new OnCompleteListener<QuerySnapshot> () {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful ()){
+                    for (QueryDocumentSnapshot documentSnapshot: task.getResult ()){
+                        usuario=documentSnapshot.toObject (Usuario.class);
+                        break;
+                    }
+                    PreferenceEntity.setEmail (usuario.getEmail ());
+                    PreferenceEntity.setName (usuario.getName ());
+                    PreferenceEntity.setRol (usuario.getRol ());
+                    PreferenceEntity.setStatus (true);
 
-        SharedPreferences sharedPref = context.getSharedPreferences(
-                getString(R.string.user_preference_key), Context.MODE_PRIVATE);
+                    PreferenceEntity.SavePreferences ();
 
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean("status", true);
-        editor.putString("email", email);
-        editor.putString("rol", rol);
-        editor.putString("name", name);
-        editor.commit();
+                    Toast.makeText (getApplicationContext (),
+                            "Login exitoso", Toast.LENGTH_SHORT).show ();
+                    Intent intent = new Intent (getApplicationContext (), ListProductsActivity.class);
+                    startActivity(intent);
+                }else{
+
+                }
+            }
+        });
     }
+
 
     public void Registrarse(View view){
         Intent intregistrarse=new Intent(this,RegistrarseActivity.class);
--- app/src/main/java/com/example/miproyecto/MainActivity.java
+++ app/src/main/java/com/example/miproyecto/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.example.miproyecto.entities.PreferenceEntity;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -29,19 +31,6 @@ public class MainActivity extends AppCompatActivity {
         tiempo.schedule (tarea, 3000);
 
     }
-    public void readUserPreferences(Context context){
-        SharedPreferences sharedPref = context.getSharedPreferences(
-                context.getString(R.string.user_preference_key), Context.MODE_PRIVATE);
-        boolean status = sharedPref.getBoolean("status",false);
-        String role = sharedPref.getString("role","usuario");
-        if(status){
-            Intent intent = new Intent(this, ListProductsActivity.class);
-            startActivity(intent);
-        }
-        else{
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
-    }
-
+    String email= PreferenceEntity.leer (1).toString ();
+    boolean status = PreferenceEntity.leer (4).equals ("true");
 }
